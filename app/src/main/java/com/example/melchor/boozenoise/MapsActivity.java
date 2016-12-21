ebc70af18d2821fb8e1861574cc1f2660f1ac260
Melchor Jérémy <melchor.pro@gmail.com>
2016-12-21 09:48:36 +0100

    probleme fragment

--- app/src/main/java/com/example/melchor/boozenoise/MapsActivity.java
+++ app/src/main/java/com/example/melchor/boozenoise/MapsActivity.java
@@ -1,62 +1,34 @@
 package com.example.melchor.boozenoise;
 
-import android.Manifest;
-import android.app.ActivityOptions;
-import android.app.FragmentManager;
+import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
-import com.example.melchor.boozenoise.utils.GetDataFromUrl;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabSelectedListener;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+public class MapsActivity extends AppCompatActivity {
 
-    private final String TAG = this.getClass().getSimpleName();
-    private double latitude, longitude;
-    private GoogleMap googleMap;
-    private int RADIUS_IN_METERS = 1000;
-
-    /**************************************/
-    /**              EVENTS              **/
-    /**************************************/
+    private static final String TAG = MapsActivity.class.getSimpleName();
+    MapsFragment mapsFragment;
+    UserProfileFragment userProfileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        // Add google maps fragment
-        //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        //mapFragment.getMapAsync(this);
+        mapsFragment = new MapsFragment();
+        userProfileFragment = new UserProfileFragment();
+        //setFragment(mapsFragment);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        MapsFragment fragment = new MapsFragment();
-        fragmentTransaction.add(R.id.fragment_container, fragment);
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container,mapsFragment);
+        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
-
-
-
-
 
         BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
         bottomBar.setItemsFromMenu(R.menu.bottom_bar_navigation, new OnMenuTabSelectedListener() {
@@ -64,17 +36,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onMenuItemSelected(int itemId) {
                 switch (itemId) {
                     case R.id.profile_item:
-                        Log.d(TAG,"itemID : " + itemId);
+                        //setFragment();
                         break;
                     case R.id.map_item:
-                        Log.d(TAG,"itemID : " + itemId);
+                        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                        fragmentTransaction.hide(userProfileFragment);
+                        //fragmentTransaction.show(mapsFragment);
+                        fragmentTransaction.add(R.id.fragment_container,mapsFragment);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
                         break;
                     case R.id.record_item:
-                        FragmentManager fragmentManager = getFragmentManager();
-                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                        UserProfileFragment fragment = new UserProfileFragment();
-                        fragmentTransaction.replace(R.id.fragment_container,fragment);
-                        fragmentTransaction.commit();
+                        setFragment(userProfileFragment);
                         break;
                 }
             }
@@ -83,52 +56,31 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         bottomBar.setActiveTabColor("#C2185B");
     }
 
-    @Override
-    public void onMapReady(GoogleMap map) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            String[] permissions = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
-            ActivityCompat.requestPermissions(this, permissions, PackageManager.PERMISSION_GRANTED);
-        }
-        this.googleMap = map;
-        googleMap.setMyLocationEnabled(true);
 
-        // Get LocationManager object from System Service LOCATION_SERVICE
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        // Create a criteria object to retrieve provider
-        Criteria criteria = new Criteria();
-
-        // Get the name of the best provider
-        String provider = locationManager.getBestProvider(criteria, true);
-
-        // Get Current Location
-        Location myLocation = locationManager.getLastKnownLocation(provider);
-
-        latitude = 43.696460;//myLocation.getLatitude();
-        longitude = 7.274179;//myLocation.getLongitude();
-
-        // Move camera to current position
-        LatLng latLng = new LatLng(latitude,longitude);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-
-        // Set listener on marker clicks
-        googleMap.setOnMarkerClickListener(this);
-
-    }
-
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        Log.d(TAG,marker.getTitle());
-        return true;
-    }
+    /**************************************/
+    /**              EVENTS              **/
+    /**************************************/
 
     /**************************************/
     /**            FUNCTIONS             **/
     /**************************************/
 
-    public void getBarsNearMe(View view) {
-        new GetDataFromUrl(latitude,longitude,RADIUS_IN_METERS,googleMap).execute();
+    /**
+     * Set a fragment in the fragment_container
+     *
+     * @param fragment the fragment we want to display
+     */
+    public void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
+
+    public void setMapFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.show(fragment);
+        fragmentTransaction.commit();
+    }
+
 }
--- app/src/main/java/com/example/melchor/boozenoise/MapsFragment.java
+++ app/src/main/java/com/example/melchor/boozenoise/MapsFragment.java
@@ -1,18 +1,147 @@
 package com.example.melchor.boozenoise;
 
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MapsFragment extends Fragment {
+import com.example.melchor.boozenoise.utils.GetDataFromUrl;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+import static android.content.ContentValues.TAG;
+import static android.content.Context.LOCATION_SERVICE;
+
+public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private MapView mapView;
+    private GoogleMap map;
+    private double latitude, longitude;
+    private int RADIUS_IN_METERS = 1000;
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /*super.onSaveInstanceState(savedInstanceState);
+        latitude = savedInstanceState.getDouble("latitude");
+        longitude = savedInstanceState.getDouble("longitude");*/
+
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
+
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) view.findViewById(R.id.fragment_maps);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets the Map
+        mapView.getMapAsync(this);
+
+        view.findViewById(R.id.getBars).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new GetDataFromUrl(latitude, longitude, RADIUS_IN_METERS, map).execute();
+            }
+        });
+
+        return view;
     }
+
+
+    /**************************************/
+    /**              EVENTS              **/
+    /**************************************/
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        if (ActivityCompat.checkSelfPermission(this.getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this.getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions(this.getActivity(), permissions, PackageManager.PERMISSION_GRANTED);
+        }
+        this.map = googleMap;
+        map.setMyLocationEnabled(true);
+
+        // Get LocationManager object from System Service LOCATION_SERVICE.
+        // We need to call getActivity because fragment doesn't extends Context
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+
+        // Create a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // Get Current Location
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        latitude = 43.696460;//myLocation.getLatitude();
+        longitude = 7.274179;//myLocation.getLongitude();
+        Log.d(TAG,"LATITUDE : 43.696460 == "+ latitude);
+        Log.d(TAG,"LONGITUDE : 7.274179 == "+ longitude);
+
+        // Move camera to current position
+        LatLng latLng = new LatLng(latitude, longitude);
+        Log.d(TAG,"LATLNG : "+latLng);
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        // Set listener on marker clicks
+        map.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putDouble("latitude", latitude);
+        outState.putDouble("longitude", longitude);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Log.d(TAG, marker.getTitle());
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    /**************************************/
+    /**            FUNCTIONS             **/
+    /**************************************/
+
 }
